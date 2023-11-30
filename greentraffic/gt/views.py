4eee567c21ca8fa090f81ba01e5bc0395e52dfd9
rina suzuki <szk_rina@outlook.jp>
2023-11-30 11:54:13 +0900

    feat:admin変更と削除

--- greentraffic/gt/views.py
+++ greentraffic/gt/views.py
@@ -38,7 +38,60 @@ def admin_top(request):
 @login_required
 def admin_map_change(request, pk):
     map_change = get_object_or_404(Map, pk=pk)
-    return render(request, 'gt/admin_map_change.html', {'map_change': map_change})
+    if request.method == 'POST':
+        form = LocationForm(request.POST)
+        if form.is_valid():
+            map_change.name = form.cleaned_data['name']
+            map_change.address = form.cleaned_data['address']
+
+            # ここで外部APIを呼び出し、JSONデータを取得
+            api_url = 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyA5diRbD4Ex24SsS0_YISzQW5f19mckhf4'
+            response = requests.get(api_url, params={'address': map_change.address})
+
+            if response.status_code == 200:
+                data = response.json()
+                location_data = data['results'][0]['geometry']['location']
+                map_change.lat = location_data['lat']  # 緯度
+                map_change.lng = location_data['lng']  # 経度
+
+                # 緯度と経度のみを含む辞書を作成
+                location_only = {'lat': map_change.lat, 'lng': map_change.lng}
+
+                # 辞書をJSONにシリアライズ
+                map_change.json_data = json.dumps(location_only)
+
+                # データベースに保存
+                map_change.save()
+
+                message_success = "データが保存されました"
+                alert_API = "APIからデータを取得できませんでした"
+                alert_form = "フォームが無効です"
+                show_modal = True
+                show_alert = True
+                return render(request, 'gt/admin_map_register.html', {
+                    'form': form,
+                    'message': message_success,
+                    'json_data': map_change.json_data,
+                    'show_modal': show_modal
+                })
+            else:
+                # APIからデータを取得できなかった場合の処理
+                return render(request, 'gt/admin_map_change.html', {
+                    'form': form,
+                    'message': alert_API,
+                    'show_alert': show_alert
+                })
+        else:
+            # フォームが無効な場合の処理
+            return render(request, 'gt/admin_map_change.html', {
+                'form': form,
+                'message': alert_form,
+                'show_alert': show_alert
+            })
+    else:
+        # GETリクエストの場合、フォームを既存のデータで初期化
+        form = LocationForm(initial={'name': map_change.name, 'address': map_change.address})
+        return render(request, 'gt/admin_map_change.html', {'form': form})
 
 # 管理者用マップ削除ビュー
 @login_required
