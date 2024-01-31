78cad5e793c75e6e812152a01d907a238012c506
rina suzuki <szk_rina@outlook.jp>
2024-01-31 12:29:56 +0900

    del:waypoint-label

--- greentraffic/static/js/pageLoadingActions.js
+++ greentraffic/static/js/pageLoadingActions.js
@@ -2,5 +2,4 @@ window.onload = function() {
     // Hide the waypoint fields and label on load
     document.getElementById('waypoint1-group').style.display = 'none';
     document.getElementById('waypoint2-group').style.display = 'none';
-    document.getElementById('waypoint-label').style.display = 'none'; // Assuming you have a label with this ID
 };
