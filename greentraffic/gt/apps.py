6b946504271b9996728b02e357c2dd7e681e69f6
rina suzuki <szk_rina@outlook.jp>
2024-02-06 14:50:20 +0900

    historyの期限を一週間に設定（）

--- greentraffic/gt/apps.py
+++ greentraffic/gt/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
-
+from django.utils import timezone
+import datetime
 
 class GtConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gt'
+
+    def ready(self):
+        from django.db.models import F
+        from .models import SearchHistory
+
+        # 1週間前の日時を計算
+        one_week_ago = timezone.now() - datetime.timedelta(weeks=1)
+
+        # 条件に合致するレコードを削除
+        SearchHistory.objects.filter(search_datetime__lt=one_week_ago).delete()
